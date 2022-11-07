daf21df571c801e61b021acddf791b2925f8ea86
najlae01 <najlae.abarghache1@gmail.com>
2022-11-07 22:06:04 +0100

    Logout Function Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Container, Nav, Navbar} from 'react-bootstrap'
+import { Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import {logout} from '../../actions/AuthAction'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const userInfo = useSelector((state) => state.authReducer.userInfo)
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -16,9 +25,19 @@ const Header = () => {
             <LinkContainer to="/cart">
             <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
             </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.username} id='username'>
+                <LinkContainer to='profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            ) :
             <LinkContainer to="/auth">
             <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
             </LinkContainer>
+            }
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
